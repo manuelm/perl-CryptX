8d717eac2c02654e711e61f50784d0814d31753b
manuel <manuel@mausz.at>
2016-06-03 17:38:14 +0200

    Add PK_CURVEOID flag to ecc_export_full

--- src/ltc/headers/tomcrypt_pk.h
+++ src/ltc/headers/tomcrypt_pk.h
@@ -3,7 +3,8 @@
 enum {
    PK_PUBLIC=0,
    PK_PRIVATE=1,
-   PK_PUBLIC_COMPRESSED=2  /* used only when exporting public ECC key */
+   PK_PUBLIC_COMPRESSED=2, /* used only when exporting public ECC key */
+   PK_CURVEOID=4           /* used only when exporting public ECC key */
 };
 
 /* Indicates standard output formats that can be read e.g. by OpenSSL or GnuTLS */
