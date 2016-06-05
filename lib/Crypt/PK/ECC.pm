ca5706718786eb29618b70a168ddb0b973651abd
manuel <manuel@mausz.at>
2016-06-05 21:04:47 +0200

    add documentation for {public,private}_short

--- lib/Crypt/PK/ECC.pm
+++ lib/Crypt/PK/ECC.pm
@@ -1027,11 +1027,25 @@ Import raw public/private key - can load data exported by L</export_key_raw>.
  #or
  my $public_der = $pk->export_key_der('public');
 
+Since CryptX-0.36 C<export_key_der> can also export keys in a format
+that does not explicitely contain curve parameters but only curve OID.
+
+ my $private_der = $pk->export_key_der('private_short');
+ #or
+ my $public_der = $pk->export_key_der('public_short');
+
 =head2 export_key_pem
 
  my $private_pem = $pk->export_key_pem('private');
  #or
  my $public_pem = $pk->export_key_pem('public');
+
+Since CryptX-0.36 C<export_key_pem> can also export keys in a format
+that does not explicitely contain curve parameters but only curve OID.
+
+ my $private_pem = $pk->export_key_pem('private_short');
+ #or
+ my $public_pem = $pk->export_key_pem('public_short');
 
 Support for password protected PEM keys
 
